827cf3e4b9c851fb3d96653af0aca9a47d761e9f
Nikita Borodulin <nborodulin.dev@gmail.com>
2024-05-28 12:37:47 +0300

    create project

---
+++ TCA-Counter/TCA_CounterApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Nikita Borodulin on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
